d0eea5ade1866c6799530ba134de4a69f41ddf3d
Mudassir5367 <mudassirhussain32303@gmail.com>
2025-04-28 17:30:04 +0500

    timeline post

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -8,12 +8,15 @@ import { MainService } from '../main.service';
 })
 export class TimelineComponent implements OnInit{
 public timelimePosts:any = [];
+public loading:boolean = true;
   constructor(
     private mainService:MainService,
   ){}
   ngOnInit(){
+    this.loading = true;
     this.mainService.getAllPosts().subscribe((res:any)=>{
       this.timelimePosts = res.data;
+      this.loading = false;
       console.log('res',this.timelimePosts);
     })
   }
